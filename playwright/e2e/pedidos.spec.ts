888334d3a2efd2f65bbe0b9747ddf6af774d9b98
Douglas cassio <dodasso.jd@gmail.com>
2026-01-29 22:54:46 -0300

    AAA - Arange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,12 +3,10 @@ import { test, expect } from '@playwright/test'
 test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
 
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   await page.getByTestId('search-order-id').fill('VLO-5NT495')
